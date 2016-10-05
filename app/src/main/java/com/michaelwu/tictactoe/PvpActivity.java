6afc6b3b7a2ee4f6b9434c63149bb8e8e9e7c6ab
wuxiaohua1011 <wuxiaohua1011@gmail.com>
2016-10-04 21:58:35 -0700

    realized that pvcomputer is impossible

--- app/src/main/java/com/michaelwu/tictactoe/PvpActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/PvpActivity.java
@@ -1,7 +1,9 @@
 package com.michaelwu.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PvpActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class PvpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pvp);
+    }
+    public void back_button_clicked(View view) {
+        startActivity(new Intent(this,MainActivity.class ));finish();
     }
 }
