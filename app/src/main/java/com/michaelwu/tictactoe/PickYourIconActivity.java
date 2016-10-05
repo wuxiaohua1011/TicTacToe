1695853153233709307b7937b21d64e0d3cd30ab
csaper6 <wuxiaohua1011@gmail.com>
2016-10-05 13:35:39 -0700

    pickyouriconacitivity added

---
+++ app/src/main/java/com/michaelwu/tictactoe/PickYourIconActivity.java
@@ -0,0 +1,13 @@
+package com.michaelwu.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickYourIconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_your_icon);
+    }
+}
