b492ac69f09e72ccf086aac661e03bb10dabc3be
csaper6 <wuxiaohua1011@gmail.com>
2016-10-07 13:26:27 -0700

    settings added

--- app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
@@ -1,7 +1,9 @@
 package com.michaelwu.tictactoe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class SettingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void activity_setting_button_player_1_clicked(View view) {
+        startActivity(new Intent(this, PickYourIconActivity.class));finish();
+    }
+
+    public void activity_setting_button_player_2_clicked(View view) {
+        startActivity(new Intent(this, PickYourIconActivity.class));finish();
     }
 }
