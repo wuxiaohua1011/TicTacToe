9c9aa44d5a086b0a5ff9460cdc4f837c5ea60f8c
csaper6 <wuxiaohua1011@gmail.com>
2016-10-11 13:02:30 -0700

    current, 10/11/2016

--- app/src/main/java/com/michaelwu/tictactoe/PickYourIconActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/PickYourIconActivity.java
@@ -1,14 +1,39 @@
 package com.michaelwu.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class PickYourIconActivity extends AppCompatActivity {
-    private Button cameraButton;
+public class PickYourIconActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button cameraButton, backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_your_icon);
+        wireWidget();
+        setListener();
+    }
+
+    private void wireWidget() {
+       backButton=(Button)findViewById(R.id.pick_your_icon_activity_button_back);
+
+    }
+
+    private void setListener() {
+        backButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.pick_your_icon_activity_button_back:startActivity(new Intent(this,SettingActivity.class));finish();break;
+            default:
+                Toast.makeText(PickYourIconActivity.this, "Something Wrong happened", Toast.LENGTH_SHORT).show();break;
+        }
+
     }
 }
--- app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
@@ -4,24 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SettingActivity extends Activity {
+public class SettingActivity extends Activity implements View.OnClickListener {
+private Button backButton, player1Button,player2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        wireWidget();
+        addListener();
     }
 
-    public void activity_setting_button_player_1_clicked(View view) {
-        startActivity(new Intent(this, PickYourIconActivity.class));finish();
+    private void wireWidget() {
+        backButton=(Button)findViewById(R.id.activity_setting_button_back);
+        player1Button=(Button)findViewById(R.id.activity_setting_button_change_player_1_icon);
+        player2Button=(Button)findViewById(R.id.activity_setting_button_change_player_2_icon);
     }
 
-    public void activity_setting_button_player_2_clicked(View view) {
-        startActivity(new Intent(this, PickYourIconActivity.class));finish();
+    private void addListener() {
+        backButton.setOnClickListener(this);
+        player1Button.setOnClickListener(this);
+        player2Button.setOnClickListener(this);
     }
 
-    public void activity_setting_button_back_clicked(View view) {
-        startActivity(new Intent(this,MainActivity.class));finish();
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.activity_setting_button_back:startActivity(new Intent(this,MainActivity.class));finish();break;
+            case R.id.activity_setting_button_change_player_1_icon:startActivity(new Intent(this,PickYourIconActivity.class));finish();break;
+            case R.id.activity_setting_button_change_player_2_icon:startActivity(new Intent(this,PickYourIconActivity.class));finish();break;
+            default:
+                Toast.makeText(SettingActivity.this, "Something is wrong", Toast.LENGTH_SHORT).show();break;
+        }
     }
 }
