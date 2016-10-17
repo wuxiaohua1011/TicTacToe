bd9e63eac49f4e5118ed699c6036a90048d24487
csaper6 <wuxiaohua1011@gmail.com>
2016-10-17 13:20:53 -0700

    clock

--- app/src/main/java/com/michaelwu/tictactoe/PvpActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/PvpActivity.java
@@ -3,37 +3,58 @@ package com.michaelwu.tictactoe;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PvpActivity extends AppCompatActivity {
-private ImageView imageView1,imageview2;
+private ImageView imageView1;
     private ImageButton temp22;
+    private TextView timeConstraintTextView;
+    private CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pvp);
         imageView1=(ImageView)findViewById(R.id.imageView1);
-        imageview2=(ImageView)findViewById(R.id.imageView2);
         temp22= (ImageButton)findViewById(R.id.PvpActivity_imageButton_22);
+        timeConstraintTextView = (TextView)findViewById(R.id.activity_pvp_textView_timeConstraint);
 
 
         temp22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences player1SharedPreferences = getSharedPreferences("player1pic",MODE_PRIVATE);
-                SharedPreferences player2SharedPreferences = getSharedPreferences("player2pic",MODE_PRIVATE);
                 String player1ImagePath = player1SharedPreferences.getString("player1pic","nothing");
                 Toast.makeText(PvpActivity.this, ""+player1ImagePath, Toast.LENGTH_SHORT).show();
                 imageView1.setBackground(Drawable.createFromPath(player1ImagePath));
             }
         });
 
+        SharedPreferences sharedPreferences = getSharedPreferences(SettingActivity.TIME_CONSTRAINT,MODE_PRIVATE);
+        final int timeConstraint = sharedPreferences.getInt(SettingActivity.TIME_CONSTRAINT,61);
+        if (timeConstraint == 61){
+            Toast.makeText(PvpActivity.this, "something went wrong", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(PvpActivity.this, ""+timeConstraint, Toast.LENGTH_SHORT).show();
+            countDownTimer = new CountDownTimer(timeConstraint*1000,1000) {
+                @Override
+                public void onTick(long l) {
+                    timeConstraintTextView.setText("Time Left: " + (int)l/1000);
+                }
 
+                @Override
+                public void onFinish() {
+                    Toast.makeText(PvpActivity.this, "Counted "+ timeConstraint, Toast.LENGTH_SHORT).show();
+                }
+            }.start();
+        }
 
 
     }
--- app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
+++ app/src/main/java/com/michaelwu/tictactoe/SettingActivity.java
@@ -3,17 +3,16 @@ package com.michaelwu.tictactoe;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 public class SettingActivity extends Activity implements View.OnClickListener {
 private Button backButton, player1Button,player2Button;
-    private ImageView player1ImageView, player2ImageView;
-
+    private NumberPicker numberPicker;
+    public static final String TIME_CONSTRAINT = "timeConstraint";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,61 +20,28 @@ private Button backButton, player1Button,player2Button;
         setContentView(R.layout.activity_setting);
         wireWidget();
         addListener();
-
-
-        SharedPreferences player1Icon = getSharedPreferences("player1pic", MODE_PRIVATE);
-        String path = player1Icon.getString("player1pic", "nothing");
-
-        if (!path.equals("nothing")) {
-            if (path.contains("sd")) {
-                player1ImageView.setBackground(Drawable.createFromPath(path));
-            } else {
-                int pathInt = Integer.parseInt(path);
-                player1ImageView.setBackgroundResource(pathInt);
-            }
-        } else {
-            Toast.makeText(SettingActivity.this, "noPicSelected", Toast.LENGTH_SHORT).show();
-        }
-
-
-
-        SharedPreferences player2Icon = getSharedPreferences("player2pic", MODE_PRIVATE);
-        String path2 = player2Icon.getString("player2pic", "nothing");
-
-        if (!path2.equals("nothing")) {
-            if (path2.contains("sd")) {
-                player2ImageView.setBackground(Drawable.createFromPath(path2));
-            } else {
-                int pathInt2 = Integer.parseInt(path2);
-                player2ImageView.setBackgroundResource(pathInt2);
-            }
-        } else {
-            Toast.makeText(SettingActivity.this, "noPicSelected", Toast.LENGTH_SHORT).show();
-        }
     }
-
-
-
 
     private void wireWidget() {
         backButton=(Button)findViewById(R.id.activity_setting_button_back);
         player1Button=(Button)findViewById(R.id.activity_setting_button_change_player_1_icon);
         player2Button=(Button)findViewById(R.id.activity_setting_button_change_player_2_icon);
-        player1ImageView = (ImageView) findViewById(R.id.activity_setting_imageView_player_1);
-        player2ImageView = (ImageView) findViewById(R.id.activity_setting_imageView_player_2);
+        numberPicker=(NumberPicker)findViewById(R.id.activity_setting_numberPicker_time);
+        numberPicker.setMaxValue(60);numberPicker.setMinValue(1);
     }
 
     private void addListener() {
         backButton.setOnClickListener(this);
         player1Button.setOnClickListener(this);
         player2Button.setOnClickListener(this);
-        player1ImageView.setOnClickListener(this);
-        player2ImageView.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(TIME_CONSTRAINT,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(TIME_CONSTRAINT,numberPicker.getValue());
         switch (view.getId()){
             case R.id.activity_setting_button_back:startActivity(new Intent(this,MainActivity.class));finish();break;
             case R.id.activity_setting_button_change_player_1_icon:startActivity(new Intent(this,PickYourIconActivityPlayer1.class));finish();break;
